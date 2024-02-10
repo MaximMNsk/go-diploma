8bd0860bacb811f6758b5c7d62323d0fac81b9c4
Maxim M <maxim.m.nsk@gmail.com>
2024-02-10 14:58:54 +0700

    App 2.5

--- server/server/main.go
+++ server/server/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/ShiraazMoollatjie/goluhn"
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5"
@@ -307,6 +308,11 @@ func (s *Server) SaveOrder(res http.ResponseWriter, req *http.Request) {
 		`select user_id from public.orders where number = $1`,
 		orderNum,
 	).Scan(&savedOrderUserID)
+
+	fmt.Println(`Errors: `)
+	fmt.Println(err)
+	fmt.Println(userID)
+	fmt.Println(orderNum)
 
 	isSavedOrder := true
 
